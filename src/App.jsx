d48c459880311aa054df313e9ca2e608586960c2
Renan <renan@minasulcorp.com>
2024-03-12 16:27:20 -0300

    Game Board completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Player from "./components/Player"
+import GameBoard from "./components/GameBoard.jsx"
+import Player from "./components/Player.jsx"
 function App() {
   
 
@@ -9,8 +10,7 @@ function App() {
           <Player initialName='Player 1' symbol='X'/>
           <Player initialName='Player 2' symbol='O'/>
         </ol>
-
-        GAME BOARD
+        <GameBoard />
       </div>
 
     LOG
